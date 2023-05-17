8eeb525d666ad179c1284a7ec3f735921d7edadc
Felipe <f.alfonso@res-ear.ch>
2023-05-17 02:47:54 -0400

    Updates

--- backups.sh
+++ backups.sh
@@ -19,7 +19,10 @@
 
 timestamp="$(date +'%b-%d-%y')"
 # eventually 'sudo'
-# tar -cf /Volumes/2TBMACSDBK/backup-${timestamp}.tar.gz /Volumes/Macintosh\ HD/Users/felipe/Downloads
+#
+# For MacOS
+# tar -zvcf /Volumes/2TBMACSDBK/backups-mac/backup-${timestamp}.tar.gz -P /Volumes/Macintosh\ HD/Users/felipe/Downloads
+
 #
 tar -cvpzf /run/media/felipe/341A-4340/backups-sdfcscx/backup-${timestamp}.tar.gz --absolute-names /home/felipe/backups
 
