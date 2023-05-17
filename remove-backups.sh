824b5a3863e8c73241cc9dbb17f66ea9d89a99f5
Felipe <f.alfonso@res-ear.ch>
2023-05-17 03:12:10 -0400

    Updates

--- remove-backups.sh
+++ remove-backups.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
+#
+#----------
+#BACKUP SYSTEM IN BASH
+#DEVELOPED BY Felipe Alfonso Gonzalez
+#f.alfonso@res-ear,ch
+#----------
+#
+#
 find "/run/media/felipe/341A-4340/backups-sdfcscx" -type f -mtime +5 -exec rm {} \;
